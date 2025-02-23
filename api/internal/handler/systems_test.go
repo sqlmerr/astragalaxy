c24416815a1b07416f0c7c0f41ef126b1471d36a
sqlmerr <sqlmerr@ya.ru>
2025-02-23 13:48:57 +0300

    feat(tests): System tests

--- api/internal/handler/systems_test.go
+++ api/internal/handler/systems_test.go
@@ -60,3 +60,46 @@ func TestGetSystemPlanets(t *testing.T) {
 		assert.Len(t, response, 0)
 	}
 }
+
+func TestGetSystemByID(t *testing.T) {
+	system, err := stateObj.SystemService.FindOneByName("initial")
+	assert.NoError(t, err)
+
+	req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/systems/%s", system.ID.String()), nil)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", userJwtToken))
+	res, err := app.Test(req, -1)
+	assert.NoError(t, err)
+
+	if assert.Equal(t, http.StatusOK, res.StatusCode) {
+		body, err := io.ReadAll(res.Body)
+		assert.NoError(t, err)
+		var response schemas.SystemSchema
+		err = json.Unmarshal(body, &response)
+		assert.NoError(t, err)
+		assert.Equal(t, "initial", response.Name)
+		assert.Equal(t, system.ID, response.ID)
+	}
+}
+
+func TestGetAllSystems(t *testing.T) {
+	system, err := stateObj.SystemService.FindOneByName("initial")
+	assert.NoError(t, err)
+
+	req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/systems"), nil)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", userJwtToken))
+	res, err := app.Test(req, -1)
+	assert.NoError(t, err)
+
+	if assert.Equal(t, http.StatusOK, res.StatusCode) {
+		body, err := io.ReadAll(res.Body)
+		assert.NoError(t, err)
+		var response []schemas.SystemSchema
+		err = json.Unmarshal(body, &response)
+		assert.NoError(t, err)
+
+		assert.Equal(t, system.ID, response[0].ID)
+		assert.Equal(t, system.Name, response[0].Name)
+	}
+}
